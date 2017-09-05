23ebc332e749432586693ece297831628a279c3b
lacsaP <patatetom@users.noreply.github.com>
2017-09-05 18:25:06 +0200

    Create dupdnp.py

---
+++ dupdnp.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+# find /path/to/search/ -type f -not -empty -printf '%p\t%s\n' | dupdnp.py
+
+# check size :
+# fill dict with size as key and list of paths as value
+sizes = {}
+with open('/dev/stdin', 'r') as lines:
+    for line in lines:
+        # line is 'path\tsize\n'
+        path, size = line.strip('\n').split('\t')
+        size = int(size)
+        sizes.setdefault(size, []).append(path)
+# remove empty files if ever
+sizes.get(0) and sizes.pop(0)
+
+
+# check header (first 1024 bytes) :
+# fill dict with (size, header) as key and list of paths as value
+headers = {}
+for size, paths in sizes.items():
+    if len(paths) > 1:
+        for path in paths:
+            with open(path, 'rb') as data:
+                header = data.read(1024)
+            headers.setdefault((size, header), []).append(path)
+# free memory
+del(sizes)
+
+
+from hashlib import md5
+
+
+# check md5 hash of extended header (first mega-byte) :
+# fill dict with (size, hash) as key and list of paths as value
+hheaders, megabyte = {}, 1024*1024
+for (size, header), paths in headers.items():
+    if len(paths) > 1:
+        for path in paths:
+            with open(path, 'rb') as data:
+                hheader = md5(data.read(megabyte)).digest()
+            hheaders.setdefault((size, hheader), []).append(path)
+# free memory
+del(headers)
+
+
+# check md5 hash :
+# fill dict with hash as key and list of paths as value
+checksums = { hheader:paths for (size, hheader), paths in hheaders.items() if size <= megabyte }
+for (size, hheader), paths in hheaders.items():
+    if len(paths) > 1 and size > megabyte:
+        for path in paths:
+            with open(path, 'rb') as data:
+                checksum = md5(data.read()).digest()
+            checksums.setdefault(checksum, []).append(path)
+# free memory
+del(hheaders)
+
+
+# write results
+for checksum, paths in checksums.items():
+    if len(paths) > 1:
+        paths.pop()
+        for path in paths: print(path)
+
+
+# cython --embed dupdnp.py
+# gcc $( python-config --cflags --libs ) dupdnp.c -o dupdnp
