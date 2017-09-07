3ba8f87f3b00a3a617b61a9e49c426789272fab0
lacsaP <patatetom@users.noreply.github.com>
2017-09-07 16:44:17 +0200

    Variable width and uniqueness test

--- dupdnp.py
+++ dupdnp.py
@@ -18,29 +18,31 @@ sizes.get(0) and sizes.pop(0)
 # remove single files
 sizes = {size: paths for size, paths in sizes.items() if len(paths) > 1}
 
-# check header (first 1024 bytes) :
+# check header :
 # fill dict headers with (size, header) as key and list of paths as value
 headers = {}
+headerWidth = 1024 * 4
 for size, paths in sizes.items():
     for path in paths:
         with open(path, 'rb') as data:
-            header = data.read(1024)
+            header = data.read(headerWidth)
         headers.setdefault((size, header), []).append(path)
 # free memory
 del(sizes)
 # remove single files
 headers = {(size, header): paths for (size, header), paths in headers.items() if len(paths) > 1}
 
-# check md5 hash of fragment (first mega-byte) :
+# check md5 hash of fragment :
 # fill dict fragments with (size, hash) as key and list of paths as value
+fragmentWidth = 1024 * 1024 * 4
 # preload files already read (and stored in memory)
-fragments = {(size, md5(header).digest()): paths for (size, header), paths in headers.items() if size < 1024 + 1}
+fragments = {(size, md5(header).digest()): paths for (size, header), paths in headers.items() if size < headerWidth + 1}
 # remove files already read and header
-headers = {size: paths for (size, header), paths in headers.items() if size > 1024}
+headers = {size: paths for (size, header), paths in headers.items() if size > headerWidth}
 for size, paths in headers.items():
     for path in paths:
         with open(path, 'rb') as data:
-            fragment = md5(data.read(1024 * 1024)).digest()
+            fragment = md5(data.read(fragmentWidth)).digest()
         fragments.setdefault((size, fragment), []).append(path)
 # free memory
 del(headers)
@@ -50,9 +52,9 @@ fragments = {(size, fragment): paths for (size, fragment), paths in fragments.it
 # check md5 hash :
 # fill dict with hash as key and list of paths as value
 # preload files already hashed (and stored in memory)
-checksums = {fragment: paths for (size, fragment), paths in fragments.items() if size < 1024 * 1024 + 1}
+checksums = {fragment: paths for (size, fragment), paths in fragments.items() if size < fragmentWidth + 1}
 # remove files already hashed and fragment
-fragments = {size: paths for (size, fragment), paths in fragments.items() if size > 1024 * 1024}
+fragments = {size: paths for (size, fragment), paths in fragments.items() if size > fragmentWidth}
 for size, paths in fragments.items():
     for path in paths:
         with open(path, 'rb') as data:
@@ -60,12 +62,12 @@ for size, paths in fragments.items():
         checksums.setdefault(checksum, []).append(path)
 # free memory
 del(fragments)
+# remove single files
+checksums = {checksum: paths for checksum, paths in checksums.items() if len(paths) > 1}
 
 # write results
 for checksum, paths in checksums.items():
-    if len(paths) > 1:
-        paths.pop()
-        for path in paths: print(path)
+    for path in paths[1:]: print(path)
 
 # cython3 --embed ./dupdnp.py
 # gcc $( python3-config --cflags --libs ) ./dupdnp.c -o ./dupdnp
