2421416fb3ddaaadbf859a3f86823b3888deec88
lacsaP <patatetom@users.noreply.github.com>
2017-09-07 09:54:23 +0200

    Direct use of previously read files

--- dupdnp.py
+++ dupdnp.py
@@ -5,7 +5,7 @@
 from hashlib import md5
 
 # check size :
-# fill dict with size as key and list of paths as value
+# fill dict sizes with size as key and list of paths as value
 sizes = {}
 with open('/dev/stdin', 'r') as lines:
     for line in lines:
@@ -19,7 +19,7 @@ sizes.get(0) and sizes.pop(0)
 sizes = {size: paths for size, paths in sizes.items() if len(paths) > 1}
 
 # check header (first 1024 bytes) :
-# fill dict with (size, header) as key and list of paths as value
+# fill dict headers with (size, header) as key and list of paths as value
 headers = {}
 for size, paths in sizes.items():
     for path in paths:
@@ -31,16 +31,21 @@ del(sizes)
 # remove single files
 headers = {(size, header): paths for (size, header), paths in headers.items() if len(paths) > 1}
 
-# check md5 hash of extended header (first mega-byte) :
-# fill dict with (size, hash) as key and list of paths as value
-hheaders, megabyte = {}, 1024*1024
-for (size, header), paths in headers.items():
+# check md5 hash of fragment (first mega-byte) :
+# fill dict fragments with (size, hash) as key and list of paths as value
+# preload files already read (and stored in memory)
+fragments = {(size, md5(header).digest()): paths for (size, header), paths in headers.items() if size < 1024 + 1}
+# remove files already read and header
+headers = {size: paths for (size, header), paths in headers.items() if size > 1024}
+for size, paths in headers.items():
     for path in paths:
         with open(path, 'rb') as data:
-            hheader = md5(data.read(megabyte)).digest()
-        hheaders.setdefault((size, hheader), []).append(path)
+            fragment = md5(data.read(1024 * 1024)).digest()
+        fragments.setdefault((size, fragment), []).append(path)
 # free memory
 del(headers)
+# remove single files
+fragments = {(size, fragment): paths for (size, fragment), paths in fragments.items() if len(paths) > 1}
 
 # check md5 hash :
 # fill dict with hash as key and list of paths as value
